b2a52d7a4be61103b571d867eb9d77bf40d9c76b
sheelamswetha <sheelamswetha5@gmail.com>
2024-09-15 18:32:28 +0530

    practice

---
+++ dict.py
@@ -0,0 +1,14 @@
+a={'color':'red','age':'22','name':'ravi'}
+print(a)
+
+#values()
+pet = {'color': 'red', 'age': 42}
+for value in pet.values():
+     print(value)
+
+#keys()
+pet={'color':'red','age':'21'}
+for key in pet.keys():
+    print(key)
+for item in pet.items():
+    print(item)
--- inheritance.py
+++ inheritance.py
@@ -4,4 +4,9 @@ class Animal:
         self.weight=b
     def display(self):
         print(self.height,self.weight)
-
+class Dog(Animal):
+    def speak(self):
+        print("Bow")
+d=Dog(2,4)
+d.display()
+d.speak()
